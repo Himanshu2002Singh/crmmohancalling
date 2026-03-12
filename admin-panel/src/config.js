112ff315ae536d1c41022f1f5eaf62337135a5bc
Priyanshu <mathur612priyanshu@gmail.com>
2026-03-12 12:52:01 +0530

    url change

--- admin-panel/src/config.js
+++ admin-panel/src/config.js
@@ -1,4 +1,5 @@
-let API_URL = "https://callingapi.trustingbrains.com/api";
+// let API_URL = "https://callingapi.trustingbrains.com/api";
+let API_URL = "https://aicallingapi.trustingbrains.com/api";
 // let API_URL = "http://localhost:5002/api"
 // export default API_URL;
 export default API_URL;
--- frontend/lib/constants/server_url.dart
+++ frontend/lib/constants/server_url.dart
@@ -1,4 +1,5 @@
-const ServerUrl = "http://192.168.1.6:5002/api";
+// const ServerUrl = "http://192.168.1.6:5002/api";
 // const ServerUrl = "http://51.20.76.55:5000/api";
 // const ServerUrl = "http://72.62.197.59:5002/api";
 // const ServerUrl = "https://callingapi.trustingbrains.com/api";
+const ServerUrl = "https://aicallingapi.trustingbrains.com/api";
